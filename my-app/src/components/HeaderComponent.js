9c13a558e8be87ed8f76824fbb21fbc2d883bb86
billio_shivam <shivammagarde512@gmail.com>
2020-07-25 10:15:17 +0530

    React Router

--- my-app/src/components/HeaderComponent.js
+++ my-app/src/components/HeaderComponent.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, Nav, NavbarToggler, Collapse, NavItem, NavbarBrand, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+        this.toggelNav = this.toggelNav.bind(this);
+    }
+
+    toggelNav() {
+        this.setState({
+            isNavOpen: !this.setState.isNavOpen
+        });
+    }
+
     render() {
         return(
             <>
-                <Navbar dark>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggelNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristornate Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span> About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contact">
+                                        <span className="fa fa-address-card fa-lg"></span> Contact
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- my-app/src/components/MainComponent.js
+++ my-app/src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent'
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -13,24 +14,29 @@ constructor(props) {
 
 	this.state = {
         dishes: DISHES,
-        selectedDish: null
 	};
 
 }
 
-onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-}
 
 render() {
+
+	const HomePage = () => {
+		return (
+			<Home />
+		);
+	}
+
 	return (
-			<div>
-				<Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
-				<Footer />
-			</div>
+		<div>
+			<Header />
+			<Switch>
+				<Route path="/home" component={HomePage} />
+				<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+				<Redirect to="/home" />
+			</Switch>
+			<Footer />
+		</div>
 		);
 	}
 }
